09475f4c60cc6529fed6752f43ef1bd33d59aecd
Rajat Patel <patelrajat394120@gmail.com>
2024-07-21 18:15:49 +0530

    Fixed Issues3

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,17 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'build', 
+    outDir: 'dist',
     rollupOptions: {
-      
-    },
-  },
-});
+      output: {
+        manualChunks: {
+          // Manually split large dependencies
+          vendor: ['react', 'react-dom']
+        }
+      }
+    }
+  }
+})
